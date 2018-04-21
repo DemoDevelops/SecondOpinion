b71c88d3b7b462dc42f5c556de82889d8adefaed
Gene Demo <demodevelops@gmail.com>
2018-04-21 17:21:46 -0400

    added react-navigation

--- App.js
+++ App.js
@@ -1,23 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import HomeScreen from './containers/HomeScreen';
+import ImageScreen from './containers/ImageScreen';
+
+const RootStack = StackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Image: {
+      screen: ImageScreen,
+    }
+  },
+  {
+    initialRouteName: 'Home',
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: '#E34B2A',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+        color: '#291425',
+        flex: 1,
+        textAlign: "center",
+      },
+    },
+  }
+);
 
 export default class App extends React.Component {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+    return <RootStack />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
